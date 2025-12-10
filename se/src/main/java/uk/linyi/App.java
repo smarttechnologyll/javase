4480fd858a98dfaa772b915140db3119cb665676
Alan <alan@example.com>
2025-12-10 21:03:23 +0800

    远程仓库绑定

--- se/src/main/java/uk/linyi/App.java
+++ se/src/main/java/uk/linyi/App.java
@@ -1,17 +1,44 @@
 package uk.linyi;
 
-import java.math.BigDecimal;
+
+import lombok.SneakyThrows;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class App {
-    public static void main( String[] args ) {
-        System.out.println(0.1+0.2);
-        System.out.println(0.3);
-        System.out.println((0.1+0.2)*2);
-        System.out.println(0.3*3);
-        System.out.println(0.9);
-        System.out.println(0.45 * 2);
-        System.out.println(BigDecimal.valueOf(0.3));
-        System.out.println(BigDecimal.valueOf(0.1 + 0.2));
-
+    @SneakyThrows
+    public static void main(String[] args) {
+        Socket socket = new Socket("127.0.0.1", 8080);
+        OutputStream os = socket.getOutputStream();
+        boolean autoflush = true;
+        PrintWriter out = new PrintWriter(
+                socket.getOutputStream(), autoflush);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+// send an HTTP request to the web server
+        out.println("GET /Servlet/hello HTTP/1.1");
+        out.println("Host: localhost:8080");
+        out.println("Connection: Close");
+        out.println();
+// read the response
+        boolean loop = true;
+        StringBuffer sb = new StringBuffer(8096);
+        while (loop) {
+            if (in.ready()) {
+                int i = 0;
+                while (i != -1) {
+                    i = in.read();
+                    sb.append((char) i);
+                }
+                loop = false;
+            }
+            Thread.currentThread().sleep(50);
+        }
+        System.out.println(sb.toString());
+        socket.close();
     }
 }
